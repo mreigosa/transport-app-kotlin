eaa76a78d04352fda626fe9f219e753471f818e5
Reigosa, Martín <martin.reigosa@mobgen.com>
2020-09-21 11:41:40 +0200

    added banner to show transport details in map screen

--- app/src/main/java/com/mreigar/transportapp/view/transportresourcemap/TransportResourceMapActivity.kt
+++ app/src/main/java/com/mreigar/transportapp/view/transportresourcemap/TransportResourceMapActivity.kt
@@ -1,11 +1,14 @@
 package com.mreigar.transportapp.view.transportresourcemap
 
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.Window
+import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import com.mreigar.transportapp.R
 import com.mreigar.transportapp.injection.injectActivity
@@ -14,8 +17,12 @@ import com.mreigar.transportapp.presentation.presenter.transportresourcemap.MapV
 import com.mreigar.transportapp.presentation.presenter.transportresourcemap.TransportResourceMapPresenter
 import com.mreigar.transportapp.presentation.presenter.transportresourcemap.TransportResourceMapViewTranslator
 import com.mreigar.transportapp.view.BaseActivity
+import kotlinx.android.synthetic.main.activity_transport_resource_map.*
 
-class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>(), TransportResourceMapViewTranslator {
+
+class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>(), TransportResourceMapViewTranslator,
+    ClusterManager.OnClusterItemClickListener<TransportResourceMapEntity>,
+    ClusterManager.OnClusterClickListener<TransportResourceMapEntity> {
 
     companion object {
         private const val ZOOM_LEVEL = 16.5f
@@ -29,6 +36,8 @@ class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>
 
     private val mapper = TransportResourceMapEntityMapper()
 
+    private var bannerExpanded: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -37,6 +46,8 @@ class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync { googleMap -> onMapReady(googleMap) }
+
+        transportMapBanner.setOnClickListener { hideTransportMapBanner() }
     }
 
     override fun getMapVisibleRegion(): MapVisibleRegion = with(mMap.projection.visibleRegion.latLngBounds) {
@@ -47,7 +58,10 @@ class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>
         mMap = googleMap
 
         clusterManager = ClusterManager(this, googleMap)
-        clusterManager.renderer = TransportResourceMarkerRenderer(this, googleMap, clusterManager)
+        clusterManager.apply {
+            renderer = TransportResourceMarkerRenderer(this@TransportResourceMapActivity, googleMap, clusterManager)
+            setOnClusterItemClickListener(this@TransportResourceMapActivity)
+        }
 
         mMap.apply {
             mapType = GoogleMap.MAP_TYPE_NORMAL
@@ -81,5 +95,41 @@ class TransportResourceMapActivity : BaseActivity<TransportResourceMapPresenter>
 
     override fun showError() {
 
+    }
+
+    override fun onClusterItemClick(item: TransportResourceMapEntity): Boolean {
+        showTransportResourceMapBanner(item.name, item.id)
+        return true
+    }
+
+    override fun onClusterClick(cluster: Cluster<TransportResourceMapEntity>?): Boolean {
+        return true
+    }
+
+    private fun showTransportResourceMapBanner(title: String, subtitle: String) {
+        transportResourceMapBannerTitle.text = title
+        transportResourceMapBannerSubtitle.text = subtitle
+
+        if (!bannerExpanded) {
+            val constraint = ConstraintSet()
+            constraint.clone(transportMapLayout)
+
+            TransitionManager.beginDelayedTransition(transportMapBanner)
+            constraint.clear(transportMapBanner.id, ConstraintSet.TOP)
+            constraint.connect(transportMapBanner.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
+            constraint.applyTo(transportMapLayout)
+            bannerExpanded = true
+        }
+    }
+
+    private fun hideTransportMapBanner() {
+        val constraint = ConstraintSet()
+        constraint.clone(transportMapLayout)
+
+        TransitionManager.beginDelayedTransition(transportMapBanner)
+        constraint.clear(transportMapBanner.id, ConstraintSet.BOTTOM)
+        constraint.connect(transportMapBanner.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
+        constraint.applyTo(transportMapLayout)
+        bannerExpanded = false
     }
 }
